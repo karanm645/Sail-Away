33d0371ba310164bb0f8ddb078721c3c33ce35f8
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2022-05-15 21:46:33 -0500

    Create edit and update actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,17 @@ class ItemsController < ApplicationController
     redirect_to "/dashboard/items"
   end 
 
+  def edit 
+    @item = Item.find(params[:id])
+  end 
+
+  def update 
+    item = Item.find(params[:id])
+    item.update(item_params)
+  
+    redirect_to "/dashboard/items"
+  end 
+
 private
   def item_params
     params.permit(:name, :category, :quantity)
