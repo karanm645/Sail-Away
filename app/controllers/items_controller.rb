828b5a59271a8001ca3748931fbd5cc9a2f06945
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2022-05-15 20:46:18 -0500

    Create action "create" with private method for params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new 
+  end 
+
+  def create 
+    item = Item.create!(item_params)
+    redirect_to '/dashboard/items'
+  end 
+
+private
+  def item_params
+    params.permit(:name, :category, :quantity)
+  end
 end 
