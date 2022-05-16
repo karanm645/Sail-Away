0e74cb63ca3497966ea9a05db1d74ea0cc42974a
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2022-05-15 19:07:08 -0500

    Rename column type to category

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'items index page' do 
   before :each do 
-    @ferrari = Item.create!(name: "Ferrari", type: "car", quantity: 10)
-    @tungsten_bracelet = Item.create!(name: "Tungsten Bracelet", type: "jewelery", quantity: 500)
-    @givenchy = Item.create!(name: "Givenchy", type: "cologne", quantity: 150)
+    @ferrari = Item.create!(name: "Ferrari", category: "Car", quantity: 10)
+    @tungsten_bracelet = Item.create!(name: "Tungsten Bracelet", category: "jewelery", quantity: 500)
+    @givenchy = Item.create!(name: "Givenchy", category: "cologne", quantity: 150)
     visit "/dashboard/items"
   end 
 
   describe 'when i visit /dashboard/items' do 
     it 'has a list of all the items' do 
       expect(page).to have_content(@ferrari.name)
-      expect(page).to have_content(@tungsten_bracelet.type)
+      expect(page).to have_content(@tungsten_bracelet.category)
       expect(page).to have_content(@givenchy.quantity)
     end 
   end 
